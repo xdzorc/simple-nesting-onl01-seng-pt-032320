f7f3a12109769566445d9787bc38cf500d64992c
DAZUN XING <dazun.xing@gmail.com>
2020-04-04 08:10:53 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -83,7 +83,8 @@ def adding_matz
           :languages => ["C"]
         }
      }
-  programmer_hash[:yukihiro_matsumoto]
+  programmer_hash[:yukihiro_matsumoto] ={:known_for => "Ruby",:languages => ["LISP", "C"]}
+  return programmer_hash
     
 end
 
